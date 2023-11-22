d8325a8815d787bf7cce9fa7063f9a37e7b071b5
Hooi Seng <nghooiseng@gmail.com>
2023-11-23 00:32:01 +0800

    update feedback ui

--- frontend/src/Feedback.jsx
+++ frontend/src/Feedback.jsx
@@ -1,29 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./container/Header";
 import Footer from "./container/Footer";
-import "./stylesheets/style.css";
+import "./stylesheets/feedback.css"; // Ensure this path is correct
 
 function Feedback() {
+  const [selectedEmotion, setSelectedEmotion] = useState(null);
+
+  const handleEmotionSelect = (emotion) => {
+    setSelectedEmotion(emotion);
+  };
   return (
-    <div>
+    <div className="feedback-wrapper">
       <Header />
-      <div class="container-feedback">
+      <div className="container-feedback">
         <form>
-          <h1>Give your Feedback</h1>
-          <div class="id">
-            <input type="text" placeholder="Full name" />
-            <i className="fas bi bi-person feedback-icon"></i>
+          <h1 className="feedback-title">Give your Feedback</h1>
+          <div className="feedback-level">
+            {["sad-tear", "frown", "meh", "smile", "grin"].map((emotion) => (
+              <button
+                key={emotion}
+                className={`level ${
+                  selectedEmotion === emotion ? "active" : ""
+                }`}
+                onClick={() => handleEmotionSelect(emotion)}
+                type="button"
+              >
+                <i className={`lar la-${emotion}`}></i>
+              </button>
+            ))}
           </div>
-          <div class="id">
+          <div className="input-group">
+            <input type="text" placeholder="Full name" />
+            <i className="las la-user feedback-icon"></i>
+          </div>
+          <div className="input-group">
             <input type="email" placeholder="Email address" />
-            <i className="fas bi bi-envelope-fill feedback-icon"></i>
+            <i className="las la-envelope feedback-icon"></i>
           </div>
           <textarea
+            className="feedback-textarea"
             cols="15"
             rows="5"
             placeholder="Enter your feedback content here..."
           ></textarea>
-          <button className="submit">Submit</button>
+          <div className="form-actions">
+            <button className="submit-btn">Submit</button>
+            <button className="cancel-btn" type="button">
+              Cancel
+            </button>
+          </div>
         </form>
       </div>
       <Footer />
